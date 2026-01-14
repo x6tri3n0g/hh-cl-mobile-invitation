fbc6b36195a0962597a8373efcd4f415c7af6714
xtring <33711323+x6tri3n0g@users.noreply.github.com>
2026-01-14 23:58:28 +0900

    feat: 인트로 이미지 슬라이드 추가 (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
