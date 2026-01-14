835e89adb5235426d69930af613b849822c45c26
xtring <hh940630@gmail.com>
2026-01-14 22:54:48 +0900

    initial commit

---
+++ app/src/lib/invitation.config.ts
@@ -0,0 +1,12 @@
+export const invitation = {
+  groom: { name: "OOO", phone: "010-0000-0000" },
+  bride: { name: "OOO", phone: "010-0000-0000" },
+  date: "2026-05-00",
+  time: "13:00",
+  venue: {
+    name: "○○웨딩홀",
+    address: "서울 ○○구 ○○로 00",
+    lat: 37.0,
+    lng: 127.0,
+  },
+};
